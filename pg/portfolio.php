9db1d96e662fd1f21c693f8909fae4f1b3f4f747
Breno Vitor <breno_070@hotmail.com>
2016-11-13 18:09:36 -0200

    INICIANDO PROJETO

---
+++ pg/portfolio.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Biografia - João da Silva</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/reset.css">
+        <link rel="stylesheet" href="../css/bio.css">
+       
+        <link rel="icon" href="../img/favicon.png">
+        <!-- carrega a fonte Crimson Text nas versões normal, itálico e negrito -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Crimson+Text:400,400italic,600">
+        <!-- carrega a fonte Open Sans Condensed na versão negrito, -->
+        <!-- que é a única necessária para os cabeçalhos            -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700">
+       
+    </head>
+    <body>
+       
+        
+        <main>
+              
+			<h1 class="titulo-principal">Sobre mim</h1>
+
+			<div class="texto">
+				<article class="bmw">
+                 
+                    <figure>
+                        <img src="../img/bmw.png" alt="Foto Site BMW">
+                        <figcaption><a href="#">BMW.com: reformulação de design e implementação de HTML, CSS e Javascript</a></figcaption>
+                    </figure>
+                 
+                </article>
+                <article class="uol">
+                     <figure>
+                        <img src="../img/uol.png" alt="Foto Site BMW">
+                           <figcaption> <a href="#">UOL.com.br: reformulação de design e implementação de HTML, CSS e Javascript</a></figcaption>
+                    </figure>
+                </article>
+                <article class="ibm">
+                     <figure>
+                         <img src="../img/ibm.png" alt="Foto Site BMW">
+                           <figcaption><a href="#">IBM.com: reformulação de design e implementação de HTML, CSS e Javascript</a></figcaption>
+                    </figure>
+                </article>
+                <article class="g1">
+                    <figure>
+                        <img src="../img/g1.png" alt="Foto Site BMW">
+                        <figcaption><a href="#">G1.com.br: reformulação de design e implementação de HTML, CSS e Javascript</a></figcaption>
+                    </figure>
+                </article>
+			</div>
+		</main>
+        <figure>
+		  <img src="../img/eu.jpg" alt="Foto de João da Silva" class="minha-foto">
+        </figure>
+		<aside class="navegacao-site">
+			<h1>João da Silva</h1>
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="portfolio.php">Portfolio</a></li>
+					<li><a href="bio.php">Sobre mim</a></li>
+					<li><a href="blog.php">Blog</a></li>
+					<li><a href="contato.php">Contato</a></li>
+				</ul>
+			</nav>
+			<ul class="icones-redes-sociais">
+				<li>
+					<a class="github" href="https://github.com/joaodasilva">
+						Github
+					</a>
+				</li>
+				<li>
+					<a class="twitter" href="https://twitter.com/joaodasilva">
+						Twitter
+					</a>
+				</li>
+				<li>
+					<a class="linkedin" href="https://br.linkedin.com/pub/joão-da-silva/32/4/508">
+						LinkedIn
+					</a>
+				</li>
+			</ul>
+		</aside>
+		<footer class="rodape-pagina">
+			&copy; João da Silva 2014
+		</footer>
+    </body>
+</html>
